d98725b962ecdb4a6ede17295c2598ff64917a74
Dmyto Haidash <dmyto.haidash@gmail.com>
2020-02-17 10:55:18 +0200

    #

--- app/Http/Middleware/SetLocale.php
+++ app/Http/Middleware/SetLocale.php
@@ -20,7 +20,7 @@ class SetLocale
         if (app('router')->currentRouteNamed('admin.*')) {
             $locale = 'ru';
         } else {
-            if (session()->has('locale')) {
+           /* if (session()->has('locale')) {
                 $locale = session()->get('locale', config('app.locale'));
             } else {
                 $locale = substr($request->server('HTTP_ACCEPT_LANGUAGE'), 0, 2);
@@ -28,7 +28,8 @@ class SetLocale
                 if ($locale !== 'ru' && $locale !== 'uk' && $locale !== 'en') {
                     $locale = 'uk';
                 }
-            }
+            }*/
+            $locale = 'en';
         }
 
         session()->put('locale', $locale);
