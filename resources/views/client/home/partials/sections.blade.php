38754cc5eea4a0f793f06b8b1b6ac0c61ea9c8c3
Dmyto Haidash <dmyto.haidash@gmail.com>
2020-02-24 11:39:36 +0200

    #

--- resources/views/client/home/partials/sections.blade.php
+++ resources/views/client/home/partials/sections.blade.php
@@ -2,7 +2,7 @@
     @foreach($sections as $section)
 
     @if ($section->children->count())
-            <h2 class="text-center text-3xl my-12 relative z-20 relative font-heading">
+            <h2 class="text-center text-3xl my-12 relative z-20 font-heading title--decorated">
                 <span>{{ $section->title }}</span>
                 {{--<div class="title-decoration inset-x-0 mx-auto w-40 h-16"></div>--}}
             </h2>
